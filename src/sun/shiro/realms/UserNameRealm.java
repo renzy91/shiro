24223f88813b551e4b72b7ac3a42b692bad0c180
2804 <2804@DESKTOP-VG2EG1D>
2016-12-12 17:36:08 +0800

    ssl

--- src/sun/shiro/realms/UserNameRealm.java
+++ src/sun/shiro/realms/UserNameRealm.java
@@ -1,20 +1,14 @@
 package sun.shiro.realms;
 
-import java.util.List;
-
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authz.AuthorizationInfo;
-import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
-import org.apache.shiro.util.ByteSource;
 
 import sun.dao.UserDAO;
-import sun.entity.Permission;
-import sun.entity.Role;
 import sun.entity.User;
 import sun.shiro.util.MyByteSource;
 
